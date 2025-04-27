5b9aaff07caec0bb87a3cf267c2679ad916d1ebb
Deltams <deltamso@bk.ru>
2025-04-27 13:30:18 +0800

    check pg-admin config

---
+++ scripts/pg-admin-scripts/auto_connect.sh
@@ -0,0 +1,17 @@
+# Подождём немного, пока поднимется Postgres (можно заменить на более сложную проверку)
+sleep 10
+
+# Авторизируемся в pgAdmin API и получаем токен
+TOKEN=$(curl -X GET \
+  http://localhost:$PGADMIN_PORT/login \
+  -H 'Content-Type: application/json' \
+  -d '{"email": "$PGADMIN_DEFAULT_EMAIL", "password": "$PGADMIN_DEFAULT_PASSWORD"}' | jq -r '.token')
+
+# Создаём сервер PostgreSQL
+SERVER_ID=$(curl -X POST \
+  http://localhost:$PGADMIN_PORT/api/server \
+  -H "Authorization: Bearer $TOKEN" \
+  -H 'Content-Type: application/json' \
+  -d @server.json | jq -r '.id')
+
+echo "Server created with ID: $SERVER_ID"
