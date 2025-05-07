f6cf6f86201b4b7a165441276fef5ee112ab8793
Deltams <deltamso@bk.ru>
2025-05-07 15:45:29 +0800

    edit script pgadmin-3

--- scripts/pg-admin-scripts/auto_connect.sh
+++ scripts/pg-admin-scripts/auto_connect.sh
@@ -1,19 +1,19 @@
 # Подождём немного, пока поднимется Postgres (можно заменить на более сложную проверку)
 echo "Starting connection script..."
-sleep 10
+# sleep 10
 
-# Авторизируемся в pgAdmin API и получаем токен
-TOKEN=$(curl -X GET \
-  http://localhost:$PGADMIN_LISTEN_PORT/login \
-  -H 'Content-Type: application/json' \
-  -d '{"email": "$PGADMIN_DEFAULT_EMAIL", "password": "$PGADMIN_DEFAULT_PASSWORD"}' | jq -r '.token')
+# # Авторизируемся в pgAdmin API и получаем токен
+# TOKEN=$(curl -X GET \
+#   http://localhost:$PGADMIN_LISTEN_PORT/login \
+#   -H 'Content-Type: application/json' \
+#   -d '{"email": "$PGADMIN_DEFAULT_EMAIL", "password": "$PGADMIN_DEFAULT_PASSWORD"}' | jq -r '.token')
 
-# Создаём сервер PostgreSQL
-echo "Token received: $TOKEN"
-SERVER_ID=$(curl -X POST \
-  http://localhost:$PGADMIN_LISTEN_PORT/api/server \
-  -H "Authorization: Bearer $TOKEN" \
-  -H 'Content-Type: application/json' \
-  -d @server.json | jq -r '.id')
+# # Создаём сервер PostgreSQL
+# echo "Token received: $TOKEN"
+# SERVER_ID=$(curl -X POST \
+#   http://localhost:$PGADMIN_LISTEN_PORT/api/server \
+#   -H "Authorization: Bearer $TOKEN" \
+#   -H 'Content-Type: application/json' \
+#   -d @server.json | jq -r '.id')
 
-echo "Server created with ID: $SERVER_ID"
+# echo "Server created with ID: $SERVER_ID"
